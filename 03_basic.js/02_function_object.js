06509d48a63435d951612efb0e351cf9544786cc
pulkitchoudhary2307 <pulkitchoudhary2307@gmail.com>
2025-06-25 23:52:24 +0530

    function call

--- 03_basic.js/02_function_object.js
+++ 03_basic.js/02_function_object.js
@@ -1,5 +1,41 @@
-function calprice(num1){
-    return num1
+// function calprice(...num1){ //... rest spred 
+//     return num1
+// }
+
+// // console.log(calprice(num1));
+
+// console.log(calprice(200,399,388,765));
+
+
+//==============================================================================================================
+
+// const user = {
+//     username : "Pulkit",
+//     price : 199,
+// }
+
+// function handleobj(anyobj){
+//     console.log(`user name is ${anyobj.username} and the price is the ${anyobj.price}`);
+// }
+
+// handleobj(user);
+
+//=========================================================================
+
+// function me ja kr change kiya 
+// handleobj({
+//     username :"pkt",
+//     price : 499
+// })
+
+//=========================================================================
+
+const myArray = [200,300,400,500]
+
+function returnsecondvalue(getArray){
+    return getArray [1]
 }
 
-console.log(calprice(num1));
+console.log(returnsecondvalue(myArray));
+
+console.log(returnsecondvalue([200,699,400,500])); // change the second index
---
+++ 03_basic.js/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log(calprice(num1));
