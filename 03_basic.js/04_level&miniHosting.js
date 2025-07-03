7bf5b63778dc27d59b2ce8f11bc7ba1e727ec72e
pulkitchoudhary2307 <pulkitchoudhary2307@gmail.com>
2025-07-03 21:45:23 +0530

    Scope level and Mini hosting

---
+++ 03_basic.js/04_level&miniHosting.js
@@ -0,0 +1,26 @@
+// function one (){
+//     const username = "hitesh"
+//     function two() {
+//         const website = "YouTube"
+//         console.log("usernamed");
+// }
+// //console.log(website);
+// two()
+// }
+// one()
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Error 
+
+// if( true){
+//     const username ="Pulkit"
+//     if(username === "Pulkit"){
+//         const website = "Pkt Studio"
+//         console.log(username+website);
+//     }
+//   //  console.log(website);
+// }
+
+// //console.log(username);
+
---
+++ 03_basic.js/05_Intergrating.js
@@ -0,0 +1,16 @@
+addone (5)
+function addone(num){
+    return num + 1
+}
+
+// expantion declartion of an variable.
+
+addtwo (5) // error because brfoe declartion
+const addtwo = function (num){
+    return num + 2 
+}
+
+
+
+
+
