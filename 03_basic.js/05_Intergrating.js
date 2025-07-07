ae9767d3dc8b9e324acd65f32c78c10802aef4d3
pulkitchoudhary2307 <pulkitchoudhary2307@gmail.com>
2025-07-07 18:50:11 +0530

    Update 05_Intergrating.js

--- 03_basic.js/05_Intergrating.js
+++ 03_basic.js/05_Intergrating.js
@@ -1,16 +1,37 @@
-addone (5)
-function addone(num){
-    return num + 1
+// addone (5)
+// function addone(num){
+//     return num + 1
+// }
+
+// // expantion declartion of an variable.
+
+// addtwo (5) // error because brfoe declartion
+// const addtwo = function (num){
+//     return num + 2 
+// }
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Arrow Function 
+
+// Agar Current me kisi ka name Change krna ho to (this) function ke wjaha se name ko change kiya ja skta hain 
+
+// {this = curent constext}
+
+
+const user ={
+    username : "pulkit",
+    price : 999,
+
+wellcomeMessage:function () {
+    console.log(`${this.username}, wellcomeMessage to wedsite`);
+    console.log(this)  //current cont       
+    
+}
 }
 
-// expantion declartion of an variable.
+user.wellcomeMessage
+user.username =" pkt Studio "
+user.wellcomeMessage()
 
-addtwo (5) // error because brfoe declartion
-const addtwo = function (num){
-    return num + 2 
-}
-
-
-
-
-
+console.log(this); 
