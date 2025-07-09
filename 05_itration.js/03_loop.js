23cb3ac236b352e5b236a262554d97be0621d00c
pulkitchoudhary2307 <pulkitchoudhary2307@gmail.com>
2025-07-09 22:57:20 +0530

    Create 03_loop.js

---
+++ 05_itration.js/03_loop.js
@@ -0,0 +1,26 @@
+//["" "" "" ""]
+//[{} {} {}]
+
+// const arr = [1,2,3,4,5,6]
+
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+const greetings = "Hello World!"
+for (const words of greetings) {
+    console.log(`Each word is ${words}`)
+}
+
+const map = new Map();
+map.set("name", "pulkit");
+map.set("age", 20);
+map.set("country", "India");
+map.set("age", 20); //that are doesnot conent dublicate value and key
+
+console.log(map);
+
+for (const [personal,details] of map) {
+    console.log(personal, ':-', details);    
+}
